2cf6f2897ecbd105617187eb89b5a1aee66355a0
MAASAHEBBI USTAD <mhsrmy1@gmail.com>
2022-02-09 03:39:34 -0500

    Add files via upload

---
+++ pq16.py
@@ -0,0 +1,13 @@
+#16)Write a Python program to find smallest number in a list.
+
+# list of numbers 
+list1 = [10, 20, 4, 45, 99,1]
+
+# sorting the list
+list1.sort()
+
+# printing the first element
+print("Smallest element is:", list1[0])
+
+
+
